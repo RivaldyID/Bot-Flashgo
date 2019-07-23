13724089507fd48d066b0cc3c6c25454eb98de4a
RivaldyID <53203176+RivaldyID@users.noreply.github.com>
2019-07-23 10:16:32 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,21 @@
+<?php
+//###################################//
+/*#*/$user_name="xxxxxxxxxx";
+/*#*/$user_gaid= "xxxxxxxxx";
+/*#*/$user_token="xxxxxxxxxxxxxxx";
+/*#*/$user_id="xxxxxxxxxxxxxxxxxxxxxx";
+/*#*/$invite_code="xxxxxxxxxxxxxx";
+/*#*/$Bearer="xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+//###################################//
+?>
+
+###########################################
+#Jangan Lupa Subscribe Channel Youtube    #
+#Admin dan aktifkan notifikasi lonceng nya#
+#karena subscribe itu gratis gak bayar👇👇#
+###########################################
+https://www.youtube.com/channel/UClb7JaAMtvIrBtRsFEbFZmg
+###########################################
+
+
+
